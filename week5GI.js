617305bd9dcef5110ad5f7791df559d0c44a797f
Governor Brown <GovernorBrown@MacBook-Pro-5.local>
2022-05-02 13:35:47 -0400

    push1

---
+++ week5GI.js
@@ -0,0 +1,12 @@
+function exercise (x){
+    return `Today's exercise ${x}`;
+}
+
+console.log(exercise('running'));
+console.log(exercise('swimming'));
+
+function pizza (y){
+    return `Each person gets ${y} amount of pizza`;
+}
+console.log(pizza(4));
+console.log(pizza(3));
