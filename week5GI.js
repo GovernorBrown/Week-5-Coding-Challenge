946e0d0978a4ff21e1edd3202313a26cc4c528be
Governor Brown <GovernorBrown@MacBook-Pro-5.local>
2022-05-02 18:47:27 -0400

    commit 2

--- week5GI.js
+++ week5GI.js
@@ -16,3 +16,69 @@ const whatever = arrowmyFunction => {
     return (x);
 };
 console.log(whatever(num1=2, num2=4));
+
+
+
+//CLASS EXPRESSION
+const PersonC1 = class {
+constructor (firstname,ssn){
+this.firstname=firstname;
+this.ssn=ssn;
+}
+getName(){
+    return this.firstname;
+}
+}
+
+const governor = new PersonC1(`Governor`,`2000`);
+const governor2 = new PersonC1(`Governor`,`2000`);
+console.log(governor.getName());
+
+// CLASS DECLARATION
+//class PersonC2 {
+
+//}
+   
+const Person2 = class {
+    constructor (name,job,age){
+        this.name=name;
+        this.job=job;
+        this.age=age;
+    class Programmer {
+        
+    constructor(givenLanguages){
+        this.name=super.name;
+        this.job=super.job;
+        this.age=super.age;
+        this.busy=true;
+        this.languages=givenLanguages;
+        }
+        completeTask(){
+        this.busy=false;
+    }
+        acceptNewTask(){
+        this.busy=true;
+        }
+        offerNewTask(){
+            if(this.busy==true){
+                console.log(`We can't take any additional requests right now`);
+            } else {
+                console.log(`How can we help?`);
+            }
+        } 
+        learnLanguage(newlanguage){
+            this.languages.push(newlanguage);
+        }
+        listLanguage(){
+            this.languages.list();
+        }
+    }
+    }
+    exercise(){
+        console.log(`Let's Workout`);
+    }
+    fetchJob(){
+        console.log(`This person's name is ${this.name} and they work for ${this.job}`);
+    }
+}
+
